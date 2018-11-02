de36aeb884addf383397fd05a51895267d787749
Florent DUBOQUET <171520@S-L-IGLAB-11.umons.ac.be>
2018-11-02 14:38:36 +0100

    test exo2

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,18 @@
 package exercise2;
 
 class Person {
+	public int age;
+	private static int computePopulationSize ;
+	private static float computeAveragePopulationAge;
+	private static int computeTotalPopulationAge;
+
+	public Person(int age) {this.age=age;computePopulationSize++;computeTotalPopulationAge += this.age;}
+
+	public static int computePopulationSize () {return computePopulationSize;}
+
+	private static float computeAveragePopulationAge (){
+		computeAveragePopulationAge =computeTotalPopulationAge/computePopulationSize;
+		return computeAveragePopulationAge;}
+
+	static void resetPopulation() {};
 }
